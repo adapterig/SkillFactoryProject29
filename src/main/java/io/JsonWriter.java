8c7333f7b7aabd5e3524ac806f84c6a4cd9d080f
Ivan Antonov <ivan.antonov.alex@gmail.com>
2022-10-30 12:17:44 +0300

    step 9

--- src/main/java/io/JsonWriter.java
+++ src/main/java/io/JsonWriter.java
@@ -9,17 +9,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class JsonWriter {
+    private static final Logger logger = Logger.getLogger(JsonWriter.class.getName());
 
     private JsonWriter() {
     }
 
     public static void writeJson(FullInfo fullInfo) {
+        logger.log(Level.INFO, "JSON writing started");
         try {
             Files.createDirectory(Paths.get("jsonReqs"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            logger.log(Level.INFO, "Directory \"jsonReqs\" created");
+        } catch (IOException ioException) {
+            logger.log(Level.FINE, "Failed to create directory, maybe directory already exists", ioException);
         }
         File jsonFile = new File("jsonReqs/req" + new Date().getTime() + ".json");
         String text = "";
@@ -31,8 +36,9 @@ public class JsonWriter {
             FileWriter fileWriter = new FileWriter(jsonFile);
             fileWriter.write(text);
             fileWriter.close();
+            logger.log(Level.INFO, "JSON file written");
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            logger.log(Level.SEVERE, "Writing JSON to file failed");
         }
     }
 }
--- src/main/java/io/XmlWriter.java
+++ src/main/java/io/XmlWriter.java
@@ -10,23 +10,33 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class XmlWriter {
+    private static final Logger logger = Logger.getLogger(XmlWriter.class.getName());
+
     private XmlWriter() {
     }
 
     public static void writeXml(FullInfo fullInfo) throws JAXBException {
-        JAXBContext jaxbContext = JAXBContext.newInstance(FullInfo.class);
-        Marshaller marshaller = jaxbContext.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        try{
-            Files.createDirectory(Paths.get("xmlReqs"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        logger.log(Level.INFO, "XML marshalling started");
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(FullInfo.class);
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            try {
+                Files.createDirectory(Paths.get("xmlReqs"));
+                logger.log(Level.INFO, "Directory \"xmlReqs\" created");
+            } catch (IOException ioException) {
+                logger.log(Level.FINE, "Failed to create directory, maybe directory already exists", ioException);
+            }
+            File xmlFile = new File("xmlReqs/req" + new Date().getTime() + ".xml");
+            marshaller.marshal(fullInfo, xmlFile);
+        } catch (JAXBException jaxbException) {
+            logger.log(Level.SEVERE, "XML marshalling failed", jaxbException);
         }
-        File xmlFile = new File("xmlReqs/req" + new Date().getTime() + ".xml");
-        marshaller.marshal(fullInfo, xmlFile);
-
+        logger.log(Level.INFO, "XML marshalled successfully");
     }
 }
 
